29f2898b98ae5214058253795087646cdeafb9a3
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2024-02-17 14:16:18 +0100

    header toggle fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,58 +3,69 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import "./header.scss"
 import { Link } from "gatsby"
-const Header = () => (
-  <nav
-    className="navbar is-fixed-top"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div className="navbar-brand">
-      <Link className="navbar-item logo" to="/">
-        <div className="navbar-item logo">
-          <StaticImage
-            src="../images/my-logo-2.png"
-            alt="logo"
-            placeholder="blurred"
-            layout="fixed"
-            width={57}
-            height={48}
-            formats={["auto", "webp", "avif"]}
-            sizes="(min-width: 800px) 48px, 100vw"
-          />
-        </div>
-      </Link>
-      <h1 className="title navbar-item is-marginless">
-        Joe Lloyd&nbsp;<span>Web Apps</span>
-      </h1>
-      <a
-        role="button"
-        className="is-hidden-desktop navbar-burger is-mobile"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navMenu"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
 
-    <div className="navbar-menu" id="navMenu">
-      <div className="navbar-end">
-        <a className="navbar-item" href="https://joe-lloyd.com/">
-          Home
-        </a>
-
+const Header = () => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  return (
+    <nav
+      className="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link className="navbar-item logo" to="/">
+          <div className="navbar-item logo">
+            <StaticImage
+              src="../images/my-logo-2.png"
+              alt="logo"
+              placeholder="blurred"
+              layout="fixed"
+              width={57}
+              height={48}
+              formats={["auto", "webp", "avif"]}
+              sizes="(min-width: 800px) 48px, 100vw"
+            />
+          </div>
+        </Link>
+        <h1 className="title navbar-item is-marginless">
+          Joe Lloyd&nbsp;<span>Web Apps</span>
+        </h1>
         <a
-          className="navbar-item"
-          href="https://joe-lloyd.com/personal-projects"
+          role="button"
+          className={`is-hidden-desktop navbar-burger is-mobile ${isOpen ? "is-active" : ""}`}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navMenu"
+          onClick={() => setIsOpen(!isOpen)}
         >
-          Personal Projects
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
         </a>
       </div>
-    </div>
-  </nav>
-)
+
+      <div className={`navbar-menu ${isOpen ? "is-active" : ""}`} id="navMenu">
+        <div className="navbar-end">
+          <a className="navbar-item" href="https://joe-lloyd.com/">
+            Home
+          </a>
+
+          <a
+            className="navbar-item"
+            href="https://joe-lloyd.com/personal-projects/"
+          >
+            Personal Projects
+          </a>
+          <a
+            className="navbar-item"
+            href="/"
+          >
+            Blog
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
 
 export default Header
