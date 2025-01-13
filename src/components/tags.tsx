38e20bb36fd041ec62f38d07cff15204c5d964e3
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2025-01-13 15:02:14 +0100

    publish new blog post

--- src/components/tags.tsx
+++ src/components/tags.tsx
@@ -1,11 +1,18 @@
 import React from "react"
-import './tags.scss'
+import "./tags.scss"
 
 const Tags: React.FC<{ tags: string[] }> = ({ tags }) => {
   return (
     <div className="tags">
       {tags.map(tag => (
-        <span key={tag} className={`tag tag-${tag.toLowerCase().replace(/ /g, '-')}`}>{tag}</span>
+        <span
+          key={tag}
+          className={`tag tag-${tag
+            .toLowerCase()
+            .replace(/ /g, "-")
+            .replace(/\+/g, "p")
+          }`}
+        >{tag}</span>
       ))}
     </div>
   )
