c37f15e625c4f24cc05a89f7179276e22588380d
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2025-02-19 19:23:13 +0100

    Added glitch effect on the image logo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -22,12 +22,28 @@ const Hero = () => {
               className="column is-justify-content-flex-end is-hidden-mobile hovering-element"
               style={{ maxWidth: "400px" }}
             >
+              <div className="glitch-shake">
+
               <StaticImage
                 src="../images/logo.png"
                 alt="logo"
                 placeholder="blurred"
                 formats={["auto", "webp", "avif"]}
               />
+              </div>
+              <div
+                style={{
+                  position: "absolute",
+                  top: "0%",
+                  left: "0%",
+                  width: "100%",
+                  height: "100%",
+                  zIndex:10,
+                  backdropFilter: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='b' x='0' y='0'%3E%3CfeFlood x='4' y='4' height='2' width='2'/%3E%3CfeComposite width='10' height='10'/%3E%3CfeTile result='a'/%3E%3CfeComposite in='SourceGraphic' in2='a' operator='in'/%3E%3CfeMorphology operator='dilate' radius='5'/%3E%3C/filter%3E%3C/svg%3E#b\")",
+                }}
+                className={"glitch"}
+              >
+              </div>
             </div>
           </div>
         </div>
